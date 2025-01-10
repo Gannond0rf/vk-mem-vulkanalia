29ee1d25a3761e947dd0362acb382eae21617021
Dave Gannon <dave.c.gannon@gmail.com>
2025-01-10 17:14:17 +0000

    AllocationInfo made Copy

--- src/definitions.rs
+++ src/definitions.rs
@@ -616,7 +616,7 @@ impl From<&AllocationCreateInfo> for ffi::VmaAllocationCreateInfo {
 }
 
 /// Parameters of `Allocation` objects, that can be retrieved using `Allocator::get_allocation_info`.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct AllocationInfo {
     /// Memory type index that this allocation was allocated from. It never changes.
     pub memory_type: u32,
